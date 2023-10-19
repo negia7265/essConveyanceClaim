36372df2662be3bdee14f0687262af19d76bde2f
panwar2001 <ayushpanwar691@gmail.com>
2023-10-19 17:31:21 +0530

    add Email listener logic

--- essAPI/EmailListener/index.js
+++ essAPI/EmailListener/index.js
@@ -1,7 +1,19 @@
-var { MailListener } = require("mail-listener5");   // NOTE: A FUTURE VERSION (release date TBA) will not require ES6 destructuring or referring to the class after the require statement (i.e. require('mail-listener6').MailListener). At this stage, this is necessary because index.js exports the MailListener class as a property of module.exports.
+const { MailListener } = require("mail-listener5");   
+const nodemailer = require('nodemailer');
+const FormData = require('form-data');
+const axios=require('axios');
+const pdfkit=require('pdfkit');
 require('dotenv').config()
 
-var mailListener = new MailListener({
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
+  }
+});
+
+const mailListener = new MailListener({
   username: process.env.EMAIL,
   password: process.env.PASSWORD,
   host: "imap.gmail.com",
@@ -13,7 +25,7 @@ var mailListener = new MailListener({
   autotls: 'never', // default by node-imap
   tlsOptions: { rejectUnauthorized: false },
   mailbox: "INBOX", // mailbox to monitor
-  searchFilter: ["ALL"], // the search filter being used after an IDLE notification has been retrieved
+  searchFilter: ["RECENT"], // Needs to be changed {https://www.npmjs.com/package/node-imap#connection-instance-methods}
   markSeen: true, // all fetched email willbe marked as seen and not fetched next time
   fetchUnreadOnStart: true, // use it only if you want to get all unread email on lib start. Default is `false`,
   attachments: false, // download attachments as they are encountered to the project directory
@@ -22,8 +34,62 @@ var mailListener = new MailListener({
 
 mailListener.start(); // start listening
 
-mailListener.on("attachment", function(attachment, path, seqno){
-    // do something with attachment
-//    let attachments=attachment['attachments'];
-    console.log(attachment.content)
-  });
+
+const  cabs=['ola','uber'];
+const checkValidMail=(subject,attachmentsCount,contentType)=>{
+  if(attachmentsCount!=1 && contentType!='application/pdf'){
+    return false;
+  }
+  const sub=subject.toLowerCase().split(' ');
+  let subLen=sub.length,cabsLen=cabs.length;
+  for(let i=0;i<subLen;i++){
+    for(let j=0;j<cabsLen;j++){
+      if(sub[i]==cabs[j]){
+        return true;
+      }
+    }
+  }
+  return false;
+}
+const sendMail=(mailOptions)=>{
+  transporter.sendMail(mailOptions, (error, info)=>{
+    if (error) {
+     console.log(error);
+     } else {
+     console.log('Email sent: ' + info.response);
+     }
+   }); 
+}
+mailListener.on("mail", (mail, seqno)=>{
+  const subject=mail.headers.get('subject');
+  const mailFrom=mail.headers.get('return-path').text;
+  const attachments=mail.attachments;
+
+ if(checkValidMail(subject,attachments[0].length,attachments[0].contentType)){
+    const pdf=attachments[0].content;
+    const formData = new FormData();
+    formData.append('file', pdf, {
+      filename: attachments[0].filename,
+      contentType: attachments[0].contentType
+    });
+    axios.post('http://127.0.0.1:5000/extractInvoice/', formData).then((res)=>{
+           const url=encodeURI(`http://localhost:5173/?amt=${res.data.amount}&Mode=${res.data.travelMode}&dis=${res.data.Distance}&src=${res.data.sourceAddress}&dest=${res.data.destinationAddress}&Date=${res.data.Date}`);           
+           const mailOptions = {
+            from: process.env.EMAIL,
+            to: mailFrom,
+            subject: 'Your Transcript had been processed successfully!',
+            text: `Hi\nClick on the following link to proceed\n${url}`
+          };
+          sendMail(mailOptions);
+       }).catch((err)=>console.error(err));
+
+  }else{
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: mailFrom,
+      subject: 'Invalid Email',
+      text: 'Hi,\nMake sure email is attached with one invoice of OLA/UBER and the subject needs to contain OLA or UBER for further processing.'
+    };
+     sendMail(mailOptions);
+  }
+});
