99d87108d4ddcb2793f92f9c8492f4627f9891e6
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-11-29 03:25:06 +0530

    changes new dashoboard

--- essClient/src/SideBar/Sidebar/index.jsx
+++ essClient/src/SideBar/Sidebar/index.jsx
@@ -29,33 +29,26 @@ export default function Sidebar({ children }) {
 
   return (
     <>
-      <SidebarContainer displaySidebar={displaySidebar}>
-        <SidebarWrapper>
-          <SidebarLogoWrapper displaySidebar={displaySidebar}>
-            <SidebarLogo href="#">
-              <span className="app-brand-logo demo">
-                <img src={BrandLogo} alt="Brand logo" />
-              </span>
-              <SidebarBrand
-                displaySidebar={displaySidebar}
-                className="app__brand__text"
-              >
-                ESS
-              </SidebarBrand>
-            </SidebarLogo>
-            <SidebarToggler
-              displaySidebar={displaySidebar}
-              onClick={handleSidebarDisplay}
-            >
-              <div className="outer__circle">
-                <div className="inner__circle" />
-              </div>
-            </SidebarToggler>
-          </SidebarLogoWrapper>
-          <SidebarItems displaySidebar={displaySidebar} />
-        </SidebarWrapper>
-      </SidebarContainer>
-      <Children displaySidebar={displaySidebar}>{children} </Children>
+    <nav>
+        <div className="container-nav">
+          <div className="logo1">
+            <img className="logo" src={BrandLogo} alt="Brand logo" />
+            <span className="logo_heading">ESS</span>
+          </div>
+          <a href="/Dashboard" className="List">
+            DashBoard
+          </a>
+          <a href="/SignUp" className="List">
+            Sign Up
+          </a>
+          <a href="/Login" className="List">
+            Sign In
+          </a>
+          <a href="#" className="List">
+            Sign Out
+          </a>
+        </div>
+      </nav>
       </>
   );
 }
